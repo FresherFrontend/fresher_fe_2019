d2bf1890276d1f049b02894be7dc04cfbe2be7ae
myvu2011 <myvu201195@gmail.com>
2019-03-19 15:04:12 +0700

    change html, css, js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+(function ($) {
+	$( "#checkin" ).datepicker({
+        dateFormat: "MM - DD - yy",
+        showOn: "both",
+    });
+    $( "#checkout" ).datepicker({
+        dateFormat: "MM - DD - yy",
+        showOn: "both",
+    });
+})(jQuery);
