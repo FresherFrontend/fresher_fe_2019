3427cec12c95a12b23d1d355f65d2db969447fc9
Xuan Hieu <xuanhieu0605@gmail.com>
2019-03-27 14:38:10 +0700

    upload workshop v1.1

---
+++ workshopv1.1/js/1.js
@@ -0,0 +1,12 @@
+﻿$(function(){
+ // $('#datepicker1,#datepicker2').datepicker();
+ 	var date = new Date();
+	    date.setDate(date.getDate());
+	    $("#datepicker1,#datepicker2").datepicker({
+	          format: "mm/dd/yyyy",
+	          orientation: "top right",
+	          todayHightinght: true,
+	          startDate: date
+	       });
+})  
+ 
