3ec82961cad9750fe0bd0a6bd7e5ec1a7dfffb3d
kha49 <42962783+kha49@users.noreply.github.com>
2019-04-01 15:08:41 +0700

    update

--- baitap bootstrap/js/main.js
+++ baitap bootstrap/js/main.js
@@ -1,21 +1,32 @@
-// $(".owl-carousel").owlCarousel({
-//   items: 3,
-//   nav: true,
-//   margin: 10,
-//   dots: false,
-//   loop: true,
-//   autoplay: true,
-//   autoplayTimeout: 3000,
-//   autoplayHoverPause: false
-// });
+$(".owl-carousel").owlCarousel({
+
+    nav: true,
+    margin: 10,
+    dots: false,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: false,
+    responsive: {
+        0: {
+            item: 1,
+            nav: true
+        },
+
+        767: {
+            item: 2,
+            nav: true
+        }
+    },
+});
 
 $(function() {
-  var date = new Date();
-  date.setDate(date.getDate());
-  $(".datetime").datepicker({
-    format: "mm/dd/yyyy",
-    orientation: "top right",
-    todayHightinght: true,
-    startDate: date
-  });
+    var date = new Date();
+    date.setDate(date.getDate());
+    $(".datetime").datepicker({
+        format: "mm/dd/yyyy",
+        orientation: "top right",
+        todayHightinght: true,
+        startDate: date
+    });
 });
