42dcdd5747d1a814594ce1419cfad87999fafcc3
kha49 <42962783+kha49@users.noreply.github.com>
2019-04-12 15:06:29 +0700

    update

--- renovation/src/app.js
+++ renovation/src/app.js
@@ -1,33 +1,14 @@
 // import './assets/styles/_app.scss';
 import Global from './views/components/global/global';
 import Header from './views/components/header/header';
+import Portfolio from './views/components/portfolio/portfolio';
+
 require('normalize.css');
 
 $(() => {
     // Code here 
     Header.onClickMenu();
     Header.onClickShowMenu();
-});
-
-$(document).ready(() => {
-    Global.lazyLoad();
-});
-
-$(window).on('resize', () => {
-    setTimeout(() => {
-        Global.lazyLoad();
-    }, 250);
-});
-
-window.onload = () => {
-
-};
-import Portfolio from './views/components/portfolio/portfolio';
-require('normalize.css');
-// require('isotope-layout');
-
-$(() => {
-    // Code here
 });
 
 $(document).ready(() => {
@@ -44,3 +25,24 @@ $(window).on('resize', () => {
 window.onload = () => {
 
 };
+// require('normalize.css');
+// // require('isotope-layout');
+
+// $(() => {
+//     // Code here
+// });
+
+// $(document).ready(() => {
+//     Global.lazyLoad();
+//     
+// });
+
+// $(window).on('resize', () => {
+//     setTimeout(() => {
+//         Global.lazyLoad();
+//     }, 250);
+// });
+
+// window.onload = () => {
+
+// };
--- renovation/src/views/components/header/header.js
+++ renovation/src/views/components/header/header.js
@@ -1,92 +1,87 @@
 const Header = {
-  toggleLanguageDropDownDesktop: () => {
-    const languageBtn = document.querySelector('#dropdownLanguageBtn');
-    const languageList = document.querySelector('#dropdownLanguageList');
-    if (languageBtn && languageList) {
-      languageBtn.addEventListener('click', (evt) => {
-        evt.preventDefault();
-        evt.stopPropagation();
-        languageList.classList.toggle('open-language');
-      });
-      document.querySelector('body').addEventListener('click', () => {
-        if (languageList.classList.contains('open-language')) languageList.classList.remove('open-language');
-      });
-    }
-  },
-  handleClickHamburgerOpenNav: () => {
-    const hamburgerBtn = document.querySelector('.navigation button.navigation__hamburger-btn');
-    const navMain = document.querySelector('.navigation nav');
-    if (hamburgerBtn && navMain) {
-      hamburgerBtn.addEventListener('click', (evt) => {
-        evt.preventDefault();
-        evt.stopPropagation();
-        navMain.classList.add('nav__is-open');
-        document.querySelector('body').classList.add('body__is__nav-open');
-        document.querySelector('html').classList.add('body__is__nav-open');
-      });
-    }
-  },
-  handleClickCloseNav: () => {
-    const closeNavs = document.querySelectorAll('.navigation .navigation__close-button button, .navigation__layout');
-    const navMain = document.querySelector('.navigation nav');
-    if (closeNavs && navMain) {
-      [].forEach.call(closeNavs, (closeNav) => {
-        closeNav.addEventListener('click', (evt) => {
-          evt.preventDefault();
-          evt.stopPropagation();
-          navMain.classList.remove('nav__is-open');
-          document.querySelector('body').classList.remove('body__is__nav-open');
-          document.querySelector('html').classList.remove('body__is__nav-open');
-        });
-      });
-    }
-  },
-<<<<<<< HEAD
-
-
-  onClickMenu: () => {
-    // window.onClickMenu = Header.onClickMenu;
-    var menuLv1s = document.querySelectorAll("span.btn_menu_lv2");
-    var menuLv2 = document.querySelector("ul.ul_menu_lv2");
-    var menuMobi = document.querySelector(".menu_mobile");
-    menuLv1s.forEach(menuLv1 => {
-      // var styleRotateLv1 = menuLv1.style.transform;
-      // var styleDisplayLv2 = menuLv2.style.display;
-      menuLv1.addEventListener('click', () => {
-        if ("rotate(180deg)" == menuLv1.style.transform) {
-          menuLv1.style.transform = "rotate(0deg)";
-          menuLv2.style.height = "0";
-          menuMobi.style.height = "389.69px";
-        } else {
-          menuLv1.style.transform = "rotate(180deg)";
-          menuLv2.style.height = "169px";
-          menuMobi.style.height = "558.69px";
-
+    toggleLanguageDropDownDesktop: () => {
+        const languageBtn = document.querySelector('#dropdownLanguageBtn');
+        const languageList = document.querySelector('#dropdownLanguageList');
+        if (languageBtn && languageList) {
+            languageBtn.addEventListener('click', (evt) => {
+                evt.preventDefault();
+                evt.stopPropagation();
+                languageList.classList.toggle('open-language');
+            });
+            document.querySelector('body').addEventListener('click', () => {
+                if (languageList.classList.contains('open-language')) languageList.classList.remove('open-language');
+            });
         }
-      });
-    })
-  },
+    },
+    handleClickHamburgerOpenNav: () => {
+        const hamburgerBtn = document.querySelector('.navigation button.navigation__hamburger-btn');
+        const navMain = document.querySelector('.navigation nav');
+        if (hamburgerBtn && navMain) {
+            hamburgerBtn.addEventListener('click', (evt) => {
+                evt.preventDefault();
+                evt.stopPropagation();
+                navMain.classList.add('nav__is-open');
+                document.querySelector('body').classList.add('body__is__nav-open');
+                document.querySelector('html').classList.add('body__is__nav-open');
+            });
+        }
+    },
+    handleClickCloseNav: () => {
+        const closeNavs = document.querySelectorAll('.navigation .navigation__close-button button, .navigation__layout');
+        const navMain = document.querySelector('.navigation nav');
+        if (closeNavs && navMain) {
+            [].forEach.call(closeNavs, (closeNav) => {
+                closeNav.addEventListener('click', (evt) => {
+                    evt.preventDefault();
+                    evt.stopPropagation();
+                    navMain.classList.remove('nav__is-open');
+                    document.querySelector('body').classList.remove('body__is__nav-open');
+                    document.querySelector('html').classList.remove('body__is__nav-open');
+                });
+            });
+        }
+    },
 
-  onClickShowMenu: () => {
-    var menu = document.querySelector(".mobile_menu");
-    var menuMobi = document.querySelector(".menu_mobile");
-    menu.onclick = () => {
-      let menu2 = document.querySelector(".mobile_menu");
-      let getClassMoiMenu2 = menu2.getAttribute("class");
-      
-      if (getClassMoiMenu2 == "mobile_menu mobile_menu_icon") {
-        menu2.setAttribute("class","mobile_menu mobile_menu_icon_x");
-        menuMobi.style.height = "389.69px";
-      }else{
-        menu2.setAttribute("class","mobile_menu mobile_menu_icon");
-        menuMobi.style.height = "0";
-      }
+
+    onClickMenu: () => {
+        // window.onClickMenu = Header.onClickMenu;
+        var menuLv1s = document.querySelectorAll("span.btn_menu_lv2");
+        var menuLv2 = document.querySelector("ul.ul_menu_lv2");
+        var menuMobi = document.querySelector(".menu_mobile");
+        menuLv1s.forEach(menuLv1 => {
+            // var styleRotateLv1 = menuLv1.style.transform;
+            // var styleDisplayLv2 = menuLv2.style.display;
+            menuLv1.addEventListener('click', () => {
+                if ("rotate(180deg)" == menuLv1.style.transform) {
+                    menuLv1.style.transform = "rotate(0deg)";
+                    menuLv2.style.height = "0";
+                    menuMobi.style.height = "389.69px";
+                } else {
+                    menuLv1.style.transform = "rotate(180deg)";
+                    menuLv2.style.height = "169px";
+                    menuMobi.style.height = "558.69px";
+
+                }
+            });
+        })
+    },
+
+    onClickShowMenu: () => {
+        var menu = document.querySelector(".mobile_menu");
+        var menuMobi = document.querySelector(".menu_mobile");
+        menu.onclick = () => {
+            let menu2 = document.querySelector(".mobile_menu");
+            let getClassMoiMenu2 = menu2.getAttribute("class");
+
+            if (getClassMoiMenu2 == "mobile_menu mobile_menu_icon") {
+                menu2.setAttribute("class", "mobile_menu mobile_menu_icon_x");
+                menuMobi.style.height = "389.69px";
+            } else {
+                menu2.setAttribute("class", "mobile_menu mobile_menu_icon");
+                menuMobi.style.height = "0";
+            }
+        }
     }
-  }
 
 };
 export default Header;
-=======
-};
-export default Header;
->>>>>>> Khanv49
