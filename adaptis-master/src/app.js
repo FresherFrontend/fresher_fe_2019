ce208ea3b1c3dbf8a432138a7456c29a86bb6f4c
HangDao <daothihang9494@gmail.com>
2019-04-17 15:38:26 +0700

    init branch hangdt

--- adaptis-master/src/app.js
+++ adaptis-master/src/app.js
@@ -1,14 +1,16 @@
 // import './assets/styles/_app.scss';
 import Global from './views/components/global/global';
 import Header from './views/components/header/header';
+import Hover from './views/components/thework/thework';
 require('normalize.css');
-
+require('../src/assets/scripts/snake');
 $(() => {
   // Code here
 });
 
 $(document).ready(() => {
   Global.lazyLoad();
+  Hover.hoverDir();
 });
 
 $(window).on('resize', () => {
